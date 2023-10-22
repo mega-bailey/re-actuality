db96e1eba3993676727ea7909d79ee7166b7d485
Megan Bailey <megan-bailey@outlook.com>
2023-10-22 15:59:07 -0700

    Navbar and footer complete

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,23 @@ import styles from '@/styles/Navbar.module.css';
 export const Navbar = () => {
   return (
     <header>
-      <div className={styles.navbar}>Re-actuality. All Rights Reserved.</div>
+      <div className={styles.navbar}>
+        <div className={styles.navbarLeft}>
+          <li className={styles.logo}>
+            <a href='#' className={styles.hover}>
+              Re-<br></br> actuality
+            </a>
+          </li>
+        </div>
+        <div className={styles.navbarRight}>
+          <a href='/about' className={styles.hover}>
+            <li>About</li>
+          </a>
+          <a href='/contact' className={styles.hover}>
+            <li>Contact</li>
+          </a>
+        </div>
+      </div>
     </header>
   );
 };
